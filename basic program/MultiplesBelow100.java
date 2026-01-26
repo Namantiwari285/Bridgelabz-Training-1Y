44484109c51b7f2499da5dba6fc16d2b1be5f68a
Namantiwari285 <namant5432@gmail.com>
2026-01-26 17:43:03 +0530

    [NAMAN] Added MultiplesBelow100.java file

---
+++ basic program/MultiplesBelow100.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MultiplesBelow100 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+
+        if (number > 0 && number < 100) {
+
+            for (int i = 100; i >= 1; i--) {
+                if (i % number == 0) {
+                    System.out.println(i);
+                    continue;
+                }
+            }
+
+        } else {
+            System.out.println("Enter positive number below 100");
+        }
+    }
+}
