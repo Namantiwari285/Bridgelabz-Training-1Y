8a06faf7b472c23e718cf76b5172651392749a50
Namantiwari285 <namant5432@gmail.com>
2026-01-26 17:38:03 +0530

    [NAMAN] Added GradeCalculator.java file

---
+++ basic program/GradeCalculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Physics marks: ");
+
+        int p = sc.nextInt();
+        System.out.print("Enter Chemistry marks: ");
+       
+	   int c = sc.nextInt();
+        System.out.print("Enter Maths marks: ");
+        int m = sc.nextInt();
+
+        double average = (p + c + m) / 3.0;
+
+        System.out.println("Average Marks = " + average);
+
+        if (average >= 80) {
+            System.out.println("Grade:A");
+            System.out.println("Remarks: Level 4 (Above agency-normalized standards)");
+        } else if (average >= 70) {
+		
+            System.out.println("Grade: B");
+            System.out.println("Remarks: Level 3");
+        
+		} else if (average >= 60) {
+            System.out.println("Grade: C");
+            System.out.println("Remarks: Level 2");
+         
+		 } else if (average >= 50) {
+            System.out.println("Grade: D");
+            System.out.println("Remarks: Level 1 (Well below standards)");
+        } else if (average >= 40)
+		{
+            System.out.println("Grade: E");
+            System.out.println("Remarks: Too below standards");
+        } else {
+            System.out.println("Grade: R");
+            System.out.println("Remarks: Remedial standards");
+        }
+    }
+}
