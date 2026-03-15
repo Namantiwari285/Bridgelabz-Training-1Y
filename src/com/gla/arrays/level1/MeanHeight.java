1ae49633c0981efccebb0d9b88379da82f370418
Namantiwari285 <namant5432@gmail.com>
2026-03-15 15:17:47 +0530

    [NAMAN]ADD D MeanHeight.java

---
+++ src/com/gla/arrays/level1/MeanHeight.java
@@ -0,0 +1,24 @@
+package com.gla.arrays.level1;
+
+import java.util.Scanner;
+
+public class MeanHeight {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double[] height = new double[11];
+        double sum = 0;
+
+        for(int i = 0; i < height.length; i++){
+            System.out.print("Enter height: ");
+            height[i] = sc.nextDouble();
+            sum = sum + height[i];
+        }
+
+        double mean = sum / height.length;
+
+        System.out.println("Mean height = " + mean);
+    }
+}
