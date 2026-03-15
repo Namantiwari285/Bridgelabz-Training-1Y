5937c93c616a803167a5d59148da81c0198aafea
Namantiwari285 <namant5432@gmail.com>
2026-03-15 15:03:43 +0530

    [NAMAN]ADDED TableProgram

---
+++ src/com/gla/arrays/level1/TableProgram.java
@@ -0,0 +1,25 @@
+package com.gla.arrays.level1;
+
+import java.util.Scanner;
+
+public class TableProgram {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        int[] table = new int[10];
+
+        for(int i = 1; i <= 10; i++){
+            table[i-1] = num * i;
+        }
+
+        for(int i = 0; i < table.length; i++){
+            System.out.println(num + " * " + (i+1) + " = " + table[i]);
+        }
+
+    }
+}
