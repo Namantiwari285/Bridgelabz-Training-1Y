397bccb0a2a5db449c1c1a1ec9d8576d4087c1ad
Namantiwari285 <namant5432@gmail.com>
2026-03-15 15:16:06 +0530

    [NAMAN]ADDED Factors.java

---
+++ src/com/gla/arrays/level1/Factors.java
@@ -0,0 +1,32 @@
+package com.gla.arrays.level1;
+
+import java.util.Scanner;
+
+public class Factors {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        int[] factors = new int[num];
+        int index = 0;
+
+        for(int i = 1; i <= num; i++){
+
+            if(num % i == 0){
+                factors[index] = i;
+                index++;
+            }
+
+        }
+
+        System.out.println("Factors are:");
+
+        for(int i = 0; i < index; i++){
+            System.out.print(factors[i] + " ");
+        }
+    }
+}
