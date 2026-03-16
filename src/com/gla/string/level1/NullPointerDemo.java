534059b1f139f8bef7a3f92436ed41aa2d8dd46a
Namantiwari285 <namant5432@gmail.com>
2026-03-16 17:12:53 +0530

    [NAMAN]added NullPointerDemo.java

---
+++ src/com/gla/string/level1/NullPointerDemo.java
@@ -0,0 +1,37 @@
+package com.gla.string.level1;
+
+public class NullPointerDemo {
+
+    public static void generateException() {
+
+        String text = null;
+
+        System.out.println(text.length());
+    }
+
+    public static void handleException() {
+
+        try {
+
+            String text = null;
+
+            System.out.println(text.length());
+
+        } catch (NullPointerException e) {
+
+            System.out.println("NullPointerException handled successfully.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+
+            generateException();
+
+        } catch (Exception e) {
+
+            handleException();
+        }
+    }
+}
